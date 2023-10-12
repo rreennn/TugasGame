9119a13e51037b435c0b88b0b1819e00b5684cf9
rreennn <116095580+rreennn@users.noreply.github.com>
2023-10-12 07:51:57 +0700

    Add files via upload

---
+++ Pertemuan2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Pertemuan2 {
+    class Program {
+        static void Main(string[] args) {
+            Car Audi = new Car();
+            Audi.details();
+
+            Motor Yamaha = new Motor();
+            Yamaha.setMerk("Yamaha");
+            Yamaha.getMerk();
+
+            Motor Honda = new Motor();
+            Honda.getMerk();
+
+        }
+    }
+    class Car {
+        public string merk = "Merah";
+        public int speed = 60;
+        public void details() {
+            Console.WriteLine("Warna Mobil : " + merk);
+            Console.WriteLine("Kecepatan Mobil : " + speed);
+        }
+    }
+
+    class Motor {
+        private string merk = "Honda";
+        public void setMerk(string newMerk) {
+            this.merk = newMerk;
+        }
+        private void outputMerk() {
+            Console.WriteLine("Merk Motor : " + this.merk);
+        }
+
+        public void getMerk() {
+            outputMerk();
+        }
+    }
+}
